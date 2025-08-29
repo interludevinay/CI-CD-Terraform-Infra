45a5e6357bffb17f9925a7b2ef8cf886739befe0
Vinay Sharma <139574707+interludevinay@users.noreply.github.com>
2025-08-29 16:31:26 +0530

    Update user.sh

--- user.sh
+++ user.sh
@@ -2,8 +2,9 @@
 
 apt-get update -y
 apt-get install docker.io -y
-apt-get install fontconfig openjdk-21-jre -y
+
 usermod -aG docker $USER
-systemctl stop docker
-systemctl start docker 
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRaIgwgBoLPDR9g+FXRTFGzCGausCblhl12r92FDN/C dev-vin@dev-vin-HP-Pavilion" > terraform-key-enc.pub
+systemctl restart docker
+usermod -aG docker jenkins
+systemctl restart jenkins
+# echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPRaIgwgBoLPDR9g+FXRTFGzCGausCblhl12r92FDN/C dev-vin@dev-vin-HP-Pavilion" > terraform-key-enc.pub
