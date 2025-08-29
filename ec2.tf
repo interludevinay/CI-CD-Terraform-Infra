034a70b401c4d54d704dc4e2ec0cec85bf59b16e
Vinay Sharma <139574707+interludevinay@users.noreply.github.com>
2025-08-29 14:32:03 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,61 +1,78 @@
 # Key-pair
 resource "aws_key_pair" "terraform-key" {
-  key_name = "terraform-key-enc"
-  public_key = file("terraform-key-enc.pub")
+  key_name   = "terraform-key-enc"
+  public_key = tls_private_key.rsa.public_key_openssh
   tags = {
     name = "terraform-slave-sg"
-  }  
+  }
 }
 
+resource "tls_private_key" "rsa" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "local_file" "TF-KEY" {
+  filename = "TF=key"
+  content = tls_private_key.rsa.private_key_pem
+}
 
 # VPC & Security group
 resource "aws_default_vpc" "terraform-vpc" {
 
 }
 resource "aws_security_group" "terraform-sg" {
-  name = "terraform-slave-sg"
-  vpc_id = aws_default_vpc.terraform-vpc.id
+  name        = "terraform-slave-sg"
+  vpc_id      = aws_default_vpc.terraform-vpc.id
   description = "Slave machine terraform implementation sg"
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "SSH open rule"
   }
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     description = "HTTP open rule"
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Flask open rule"
+  }
+
   egress {
     from_port = 0
-    to_port = 0
-    protocol = -1
-  }  
+    to_port   = 0
+    protocol  = -1
+  }
   tags = {
     name = "terraform-slave-sg"
-  }  
+  }
 }
 
 
 # EC2
 resource "aws_instance" "slave-machine" {
-  ami = "ami-0f918f7e67a3323f0"
+  ami               = "ami-0f918f7e67a3323f0"
   availability_zone = "ap-south-1a"
-  instance_type = "t2.micro"
+  instance_type     = "t2.micro"
   root_block_device {
     volume_size = 8
     volume_type = "gp3"
   }
-  key_name = aws_key_pair.terraform-key.key_name
+  key_name        = aws_key_pair.terraform-key.key_name
   security_groups = [aws_security_group.terraform-sg.name]
   user_data = file("user.sh")
   tags = {
     name = "terraform-slave-sg"
-  }  
+  }
 }
